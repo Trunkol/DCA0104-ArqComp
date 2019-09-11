a2042b1de369433483fad658f7c1203e1b2b6ffa
Emerson Costa <costtasemerson@gmail.com>
2019-09-11 07:22:04 -0300

    Create exercicio1.asm

---
+++ exercicio1.asm
@@ -0,0 +1,46 @@
+# Emerson Costa Silva
+# Matricula: 20170039954
+
+.text
+
+main:
+	#Impressão de mensagem 1 no console
+	addi $v0, $zero, 4
+	la $a0, msg1
+	syscall
+	
+	#Leitura de dados 
+	addi $v0, $zero, 5
+	syscall
+	
+	add $t0, $v0, $zero
+	
+	#Impressão de mensagem 2 no console
+	addi $v0, $zero, 4
+	la $a0, msg2
+	syscall
+	
+	#Leitura de dados 
+	addi $v0, $zero, 5
+	syscall
+	
+	add $t1, $v0, $zero
+	
+	#Impressão de mensagem 2 no console
+	addi $v0, $zero, 4
+	la $a0, msg3
+	syscall
+	
+	#Impressão de mensagem 3 no console
+	addi $v0, $zero, 1
+	add $a0, $t0, $t1
+	syscall
+	
+	addi $v0, $zero, 10
+	syscall
+
+.data
+	msg1: .asciiz "\n digite A: "
+	msg2: .asciiz "\n digite B: "
+	msg3: .asciiz "\n A + B = "
+	
